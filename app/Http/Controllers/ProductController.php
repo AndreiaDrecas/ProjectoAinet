c0264951a13a227def9b72dd889f2c8b98b6df77
André Monteiro <2130156@my.ipleiria.pt>
2016-05-31 16:15:30 +0100

    Hello

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -12,7 +12,7 @@ class ProductController extends Controller
 {
 	public function __construct()
 	{
-		$this->middleware('auth', ['except' => 'listProducts',]);
+		$this->middleware('auth', ['except' => 'listProducts', ]);
 	}
 
     public function listProducts()
