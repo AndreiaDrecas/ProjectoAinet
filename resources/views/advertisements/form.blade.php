be00db7f233ca1d585d08b1fcdbd00748f7f6f02
André Monteiro <2130156@my.ipleiria.pt>
2016-06-17 16:29:45 +0100

    indentação

--- resources/views/advertisements/form.blade.php
+++ resources/views/advertisements/form.blade.php
@@ -1,40 +1,39 @@
+<div class="form-group">
+    {!! Form::label('name', 'Name:') !!}
+    {!! Form::text('name', null,['class' => 'form-control']) !!}
+</div>
 
- <div class="form-group">
-        {!! Form::label('name', 'Name:') !!}
-        {!! Form::text('name', null,['class' => 'form-control']) !!}
-    </div>
+<div class="form-group">
+    {!! Form::label('description', 'Description:') !!}
+    {!! Form::text('description', null,['class' => 'form-control']) !!}
+</div>
 
-    <div class="form-group">
-        {!! Form::label('description', 'Description:') !!}
-        {!! Form::text('description', null,['class' => 'form-control']) !!}
-    </div>
+<div class="form-group">
+    {!! Form::label('price', 'Price:') !!}
+    {!! Form::text('price_cents', null,['class' => 'form-control']) !!}
+</div>
 
-    <div class="form-group">
-        {!! Form::label('price', 'Price:') !!}
-        {!! Form::text('price_cents', null,['class' => 'form-control']) !!}
-    </div>
+<div class="form-group">
+    {!! Form::label('tradepref', 'Trade Preference:') !!}
+    {!! Form::text('trade_prefs', null,['class' => 'form-control']) !!}
+</div>
 
-    <div class="form-group">
-        {!! Form::label('tradepref', 'Trade Preference:') !!}
-        {!! Form::text('trade_prefs', null,['class' => 'form-control']) !!}
-    </div>
+<div class="form-group">
+    {!! Form::label('quantity', 'Quantity:') !!}
+    {!! Form::text('quantity', null,['class' => 'form-control']) !!}
+</div>
 
-    <div class="form-group">
-        {!! Form::label('quantity', 'Quantity:') !!}
-        {!! Form::text('quantity', null,['class' => 'form-control']) !!}
-    </div>
+<div class="form-group">
+    {!! Form::label('available_on', 'Available on:') !!}
+    {!! Form::input('date', 'available_on', date('Y-m-d'),['class' => 'form-control']) !!}
+</div>
 
-    <div class="form-group">
-        {!! Form::label('available_on', 'Available on:') !!}
-        {!! Form::input('date', 'available_on', date('Y-m-d'),['class' => 'form-control']) !!}
-    </div>
+<div class="form-group">
+    {!! Form::label('tag_list', 'Tags:') !!}
+    {!! Form::select('tag_list[]', $tags, null,['class' => 'form-control', 'multiple']) !!}
+</div>
 
-    <div class="form-group">
-        {!! Form::label('tag_list', 'Tags:') !!}
-        {!! Form::select('tag_list[]', $tags, null,['class' => 'form-control', 'multiple']) !!}
-    </div>
-
-    <div class="form-inline">
+<div class="form-inline">
     {!! Form::submit($submitButtonText,['class' => 'btn btn-primary form-control'] ) !!}
-        <a class="btn btn-danger" href="{{url('/')}}">Cancel</a>
-    </div>
+    <a class="btn btn-danger" href="{{url('/')}}">Cancel</a>
+</div>
--- resources/views/advertisements/list.blade.php
+++ resources/views/advertisements/list.blade.php
@@ -7,56 +7,56 @@
     <a class="btn btn-primary" href="{{ url('advertisements/create') }}">Create advertisement</a>
 </div>
 
-<br />
+<br/>
 @if (count($advertisements))
-<table class="table table-striped">
-    <thead>
-        <tr>
-            <th>name</th>
-            <th>description</th>   
-            <th>created_at</th>         
-            <th>Owner</th>
-            <th>Tags:</th>
-            <th>Actions</th>
-        </tr>
-    </thead>
-    <tbody>
-        @foreach ($advertisements as $advertisement)
-        <tr>
-            <td><a href="{{ url('advertisements', $advertisement->id) }}">{{ $advertisement->name }}</a></td>
-            <td>{{ $advertisement->description }}</td>             
-            <td>{{ $advertisement->created_at }}</td> 
-            <td>{{ $advertisement->owner_id }} </td>  
-            <td> @foreach ($advertisement->tags as $tag)
-                {{ $tag->name }}
-                @endforeach      
-            </td>          
-            <td>
-                <a class="btn btn-xs btn-primary" href="{{route('advertisements.edit', ['id' => $advertisement->id])}}">Edit</a>
+    <table class="table table-striped">
+        <thead>
+            <tr>
+                <th>name</th>
+                <th>description</th>   
+                <th>created_at</th>         
+                <th>Owner</th>
+                <th>Tags:</th>
+                <th>Actions</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($advertisements as $advertisement)
+                <tr>
+                    <td><a href="{{ url('advertisements', $advertisement->id) }}">{{ $advertisement->name }}</a></td>
+                    <td>{{ $advertisement->description }}</td>             
+                    <td>{{ $advertisement->created_at }}</td> 
+                    <td>{{ $advertisement->owner_id }} </td>  
+                    <td>
+                        @foreach ($advertisement->tags as $tag)
+                            {{ $tag->name }}
+                        @endforeach
+                    </td>      
+                              
+                    <td>
+                    <a class="btn btn-xs btn-primary" href="{{route('advertisements.edit', ['id' => $advertisement->id])}}">Edit</a>
 
-                {{ Form::open(['route' => ['advertisements.destroy',  $advertisement->id], 'method' => 'delete', 'class' => 'inline']) }}        
+                    {{ Form::open(['route' => ['advertisements.destroy',  $advertisement->id], 'method' => 'delete', 'class' => 'inline']) }}        
                 
-                <div class="form-group">
-                    <button type="submit" class="btn btn-xs btn-danger">Delete</button>
-                </div>
+                    <div class="form-group">
+                        <button type="submit" class="btn btn-xs btn-danger">Delete</button>
+                    </div>
 
-                {{ Form::close() }}
+                    {{ Form::close() }}
+                    {{ Form::open(['route' => ['advertisements.block',  $advertisement->id], 'method' => 'post', 'class' => 'inline']) }}     
 
-                {{ Form::open(['route' => ['advertisements.block',  $advertisement->id], 'method' => 'post', 'class' => 'inline']) }}     
+                    @if ($advertisement->blocked == 1)
+                        <button type="submit" class="btn btn-xs btn-danger">Blocked</button>
+                    @else
+                        <button type="submit" class="btn btn-xs btn-success">Block</button>
+                    @endif
 
-                @if ($advertisement->blocked == 1)
-                <button type="submit" class="btn btn-xs btn-danger">Blocked</button>
-                @else
-                <button type="submit" class="btn btn-xs btn-success">Block</button>
-                @endif
-
-                {{ Form::close() }}
-
-            </td>
-        </tr>
-        @endforeach
+                    {{ Form::close() }}
+                    </td>
+                </tr>
+            @endforeach
     </table>
-    @else
+@else
     <h2>No advertisement found</h2>
-    @endif
-    @endsection
+@endif
+@endsection
