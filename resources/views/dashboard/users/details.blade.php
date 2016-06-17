23475f2cbb70728ad96610f99e94b9e78a353578
André Monteiro <2130156@my.ipleiria.pt>
2016-06-17 17:39:40 +0100

    indentação

--- resources/views/dashboard/users/details.blade.php
+++ resources/views/dashboard/users/details.blade.php
@@ -8,25 +8,26 @@
 </div>
 
 @if (count($user))
-    
-            <td>{{ $user->name }}</td>
-            <td>{{ $user->email }}</td>
-             <th>
-            @if ($user->admin==1) Yes @else No @endif
-
-            </th>
-            <td>{{ $user->created_at }}</td>            
-            <td>
-                <a class="btn btn-xs btn-primary" href="{{route('users.edit', ['id' => $user->id])}}">Edit</a>
-                <form action="{{route('users.delete', ['id' => $user->id])}}" method="post" class="inline">
-                    <div class="form-group">
-                        <button type="submit" class="btn btn-xs btn-danger">Delete</button>
-                    </div>
-
-                </form>
-            </td>
-        </tr>
-    </table>
+    <td>{{ $user->name }}</td>
+    <td>{{ $user->email }}</td>
+        <th>
+            @if ($user->admin==1) 
+                Yes 
+            @else 
+                No 
+            @endif
+        </th>
+    <td>{{ $user->created_at }}</td>            
+    <td>
+        <a class="btn btn-xs btn-primary" href="{{route('users.edit', ['id' => $user->id])}}">Edit</a>
+        <form action="{{route('users.delete', ['id' => $user->id])}}" method="post" class="inline">
+            <div class="form-group">
+                <button type="submit" class="btn btn-xs btn-danger">Delete</button>
+            </div>
+        </form>
+    </td>
+</tr>
+</table>
 @else
     <h2>No user found</h2>
 @endif
