7ff541bbd1dca0a72024a69e50b964b5a4b74214
André Monteiro <2130156@my.ipleiria.pt>
2016-06-17 17:03:35 +0100

    indentação

--- resources/views/common/footer.blade.php
+++ resources/views/common/footer.blade.php
@@ -1,5 +1,5 @@
 <footer class="footer">
-  <div class="container wrap">
-    <p class="copyright text-muted small">Copyright &copy; Urban Farmers' Market 2016. All Rights Reserved</p>
-  </div>
+ 	<div class="container wrap">
+    	<p class="copyright text-muted small">Copyright &copy; Urban Farmers' Market 2016. All Rights Reserved</p>
+  	</div>
 </footer>
