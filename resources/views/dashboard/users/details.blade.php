8801742f9c77c2fd4b5376154d949e7439959523
André Monteiro <2130156@my.ipleiria.pt>
2016-06-17 18:07:48 +0100

    indentação

--- resources/views/dashboard/users/details.blade.php
+++ resources/views/dashboard/users/details.blade.php
@@ -20,7 +20,8 @@
     <td>{{ $user->created_at }}</td>            
     <td>
         <a class="btn btn-xs btn-primary" href="{{route('users.edit', ['id' => $user->id])}}">Edit</a>
-        <form action="{{route('users.delete', ['id' => $user->id])}}" method="post" class="inline">
+        <form action="{{route('users.delete', ['id' => $user->id])}}" method="post" 
+            class="inline">
             <div class="form-group">
                 <button type="submit" class="btn btn-xs btn-danger">Delete</button>
             </div>
