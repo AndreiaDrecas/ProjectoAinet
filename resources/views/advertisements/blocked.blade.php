5889d504ecc67ed8e06f92120f63ec63eab9fc37
André Monteiro <2130156@my.ipleiria.pt>
2016-06-17 22:13:46 +0100

    indentação

--- resources/views/advertisements/blocked.blade.php
+++ resources/views/advertisements/blocked.blade.php
@@ -7,46 +7,46 @@
 <h2>Blocked Advertisements</h2>
 <br></br>
 @if (count($advertisements))
-<table class="table table-striped">
-    <thead>
-        <tr>
-            <th>name</th>
-            <th>description</th>   
-            <th>created_at</th>         
-            <th>Owner</th>
-            <th>Tags:</th>
-            <th>Actions</th>
-        </tr>
-    </thead>
+    <table class="table table-striped">
+        <thead>
+            <tr>
+                <th>name</th>
+                <th>description</th>   
+                <th>created_at</th>         
+                <th>Owner</th>
+                <th>Tags:</th>
+                <th>Actions</th>
+            </tr>
+        </thead>
 
-    @foreach ($advertisements as $advertisement)
-    @if (Auth::user()->admin == 1)
-    @if ($advertisement->blocked == 1)
-    <tr>
-        <td><a href="{{ url('advertisements', $advertisement->id) }}">{{ $advertisement->name }}</a></td>
-        <td>{{ $advertisement->description }}</td>             
-        <td>{{ $advertisement->created_at }}</td> 
-        <td>{{ $advertisement->owner_id }} </td>  
-        <td> @foreach ($advertisement->tags as $tag)
-            {{ $tag->name }}
-            @endforeach     
-        </td>          
-        <td>
-        {{ Form::open(['route' => ['advertisements.block',  $advertisement->id], 'method' => 'post', 'class' => 'inline']) }}     
+        @foreach ($advertisements as $advertisement)
+            @if (Auth::user()->admin == 1)
+                @if ($advertisement->blocked == 1)
+                    <tr>
+                        <td><a href="{{ url('advertisements', $advertisement->id) }}">{{ $advertisement->name }}</  a></td>
+                        <td>{{ $advertisement->description }}</td>             
+                        <td>{{ $advertisement->created_at }}</td> 
+                        <td>{{ $advertisement->owner_id }} </td>  
+                        <td> 
+                            @foreach ($advertisement->tags as $tag)
+                                {{ $tag->name }}
+                            @endforeach     
+                        </td>          
+                        <td>
+                            {{ Form::open(['route' => ['advertisements.block',  $advertisement->id], 'method' => 'post', 'class' => 'inline']) }}     
 
-        @if ($advertisement->blocked == 1)
-        <div class="form-inline">
-        <button type="submit" class="btn btn-xs btn-danger">Blocked</button>
-        </div>
-    </td>
-        @endif
-     
-        {{ Form::close() }}
-    </td>
-</tr>
-@endif
-@endif
-@endforeach
-</table>
+                            @if ($advertisement->blocked == 1)
+                            <div class="form-inline">
+                                <button type="submit" class="btn btn-xs btn-danger">Blocked</button>
+                            </div>
+                        </td>
+                            @endif
+         
+                        {{ Form::close() }}
+                    </tr>
+                @endif
+            @endif
+        @endforeach
+    </table>
 @endif
 @endsection
